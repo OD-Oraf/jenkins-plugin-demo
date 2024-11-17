6d91ff27c92c944861f92168bf4ea1fd4fce72fa
OD Oraf <poseidonog@gmail.com>
2024-11-17 17:25:52 -0500

    get absolute path of file print contents to the console

--- src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -1,19 +1,35 @@
 package io.jenkins.plugins.sample;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.FilePath;
 import hudson.model.*;
+//import hudson.remoting.Request;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import hudson.util.FormValidation;
+
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 
 import jenkins.model.Jenkins;
 import jenkins.tasks.SimpleBuildStep;
-import org.apache.tools.ant.taskdefs.Parallel;
+//import okhttp3.*;
+import okhttp3.*;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
@@ -25,6 +41,9 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
     private boolean useFrench;
     private final String filePath;
     private CustomLogger logger;
+    private String clintId;
+    private String clientSecret;
+    private String accessToken = "29bf7e31-bc7d-4245-a19c-9b1af42b2a04";
 
     @DataBoundConstructor
     public HelloWorldBuilder(String name, String filePath) {
@@ -36,10 +55,6 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         return name;
     }
 
-    public boolean isUseFrench() {
-        return useFrench;
-    }
-
     public String getFilePath() {
         return filePath;
     }
@@ -49,17 +64,6 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         this.useFrench = useFrench;
     }
 
-//    @Override
-//    public void perform(Run<?, ?> run, FilePath workspace, EnvVars env, Launcher launcher, TaskListener listener)
-//            throws InterruptedException, IOException {
-//        if (useFrench) {
-//            listener.getLogger().println("Bonjour, " + name + "!");
-//        } else {
-//            listener.getLogger().println("Hello, " + name + "!");
-//        }
-//
-//
-//    }
     @Override
     public void perform(Run<?, ?> run, FilePath workspace, EnvVars env, Launcher launcher, TaskListener listener) throws IOException, InterruptedException {
         listener.getLogger().println("workspace: " + workspace);
@@ -78,42 +82,151 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         String agentName = getAgentName(run, env, listener);
 
         String workspacePath = workspace.getRemote();
-        String completeFilePath = workspacePath + "/" + filePath;
-        logMessage(listener, "complete file path: " + completeFilePath);
+        String absoluteFilePath = getRemoteFilePath(listener, agentName, workspacePath + "/" + filePath);
+        logMessage(listener, "Absolute File Path: " + absoluteFilePath);
 
-        printFileContents(listener, completeFilePath, agentName);
+        printFileContents(listener, absoluteFilePath);
+
+//        populateCategories(listener, absoluteFilePath, agentName, this.accessToken);
     }
 
-    private void logMessage(TaskListener listener, String message) {
+    private static void logMessage(TaskListener listener, String message) {
         listener.getLogger().println(message);
     }
 
-    private void printFileContents(TaskListener listener, String filePath, String agentName) throws IOException, InterruptedException {
+    private static String getRemoteFilePath(TaskListener listener,String agentName, String filePath) {
+        Jenkins jenkins = Jenkins.get();
+        Node agent = jenkins.getNode(agentName);
+
+        // Search File path on agent
+        FilePath remoteFile;
+
+        if (agent != null) {
+            remoteFile = new FilePath(agent.getChannel(), filePath);
+            logMessage(listener, "remoteFile: " + remoteFile);
+        } else {
+            remoteFile = null;
+            logMessage(listener, "Agent Node found in getRemoteFilePath()");
+        }
+
+
+        return remoteFile.getRemote();
+    }
+
+
+    private void printFileContents(TaskListener listener, String filePath) throws IOException, InterruptedException {
+        FilePath file = new FilePath(new File(filePath));
+        if (file.exists()) {
+            try {
+
+                logMessage(listener, "Printing file contents of " + file);
+                logMessage(listener, file.readToString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            logMessage(listener, "File " + filePath + " does not exist");
+        }
+
+    }
+
+    private static void populateCategories(
+            TaskListener listener,
+            String filePath,
+            String agentName,
+            String accessToken
+    ) throws JsonProcessingException {
+
         Jenkins jenkins = Jenkins.get();
         Node agent = jenkins.getNode(agentName);
 
         Computer computer = jenkins.getComputer(agentName);
+        FilePath remoteFile = new FilePath(agent.getChannel(), filePath);
+        String remoteFilePath = remoteFile.getRemote();
 
-        if (agent != null) {
-            FilePath remoteFile = new FilePath(agent.getChannel(), filePath);
-            if (remoteFile.exists()) {
-                try {
-                    String fileContents = remoteFile.readToString();
-                    System.out.println("File contents on agent " + agentName + ":");
-                    System.out.println(fileContents);
-                    logMessage(listener, "Printing file contents of " + filePath);
-                    logMessage(listener, fileContents);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else {
-                System.out.println("File " + filePath + " does not exist on agent " + agentName);
-                logMessage(listener, "File " + filePath + " does not exist on agent " + agentName);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+//        System.out.println("Current working directory: " + System.getProperty("user.dir"));
+        File file = new File(remoteFilePath);
+
+        try {
+            List<HashMap<String, Object>> categoriesList = mapper.readValue(new File(file.getPath()), new TypeReference<List<HashMap<String, Object>>>() {});
+            // Print the content
+//            System.out.println(categoriesList.toString());
+            for (Map<String, Object> categoryMap : categoriesList) {
+                String tagKey = categoryMap.get("tagKey").toString();
+                String urlEncodedTagKey = urlEncode(tagKey);
+                String value = categoryMap.get("value").toString();
+
+                OkHttpClient client = new OkHttpClient().newBuilder().build();
+                MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+
+                // Create JSON object
+                ObjectNode jsonObject = mapper.createObjectNode();
+//                jsonObject.put("tagValue", tagKey);
+
+                // Create JSON array and add the value
+                ArrayNode arrayNode = mapper.createArrayNode();
+                arrayNode.add(value);
+
+                // Add the array to the JSON object
+                jsonObject.set("tagValue", arrayNode);
+
+                logMessage(listener ,jsonObject.toString());
+
+                // Create Request Body
+                RequestBody body = RequestBody.create(mediaType, jsonObject.toString());
+
+                // Get Latest Asset Version
+                String latestVersion = getLatestVersion(listener);
+
+                // Build Request
+                Request request = new Request.Builder()
+                        .url("https://anypoint.mulesoft.com/exchange/api/v2/assets/a541ecba-4afe-4ce2-a4bb-7c8849912c7f/deom/" + latestVersion + "/tags/categories/" + urlEncodedTagKey)
+                        .method("PUT", body)
+                        .addHeader("Content-Type", "application/json")
+                        .addHeader("Authorization", "bearer " + accessToken)
+                        .build();
+                Response response = client.newCall(request).execute();
+
+                // Log request details
+                logMessage(listener, "Request URL: " + request.url());
+                logMessage(listener, "Request Method: " + request.method());
+                logMessage(listener,"Request Headers: " + request.headers());
+
+                // Log response details
+                logMessage(listener, "Response Status Code: " + response.code());
+                logMessage(listener, "Response Headers: " + response.headers());
+                logMessage(listener,"Response Body: " + response.body().string());
+
+                // Close the response body
+                response.close();
             }
-        } else {
-            System.out.println("Agent " + agentName + " not found");
-            logMessage(listener, "Agent " + agentName + " not found");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static String urlEncode(String url) {
+        url = url.replace(" ", "%20").trim();
+        return url;
+    }
+
+
+
+    private static String getFileDetailsFromServer(TaskListener listener, String accessToken) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, "");
+        Request request = new Request.Builder()
+                .url("https://anypoint.mulesoft.com/exchange/api/v2/assets/a541ecba-4afe-4ce2-a4bb-7c8849912c7f/deom/asset")
+                .method("GET", body)
+                .addHeader("Authorization", "bearer " + accessToken)
+                .build();
+        Response response = client.newCall(request).execute();
+        return "";
     }
 
     private String getAgentName(Run<?, ?> run, EnvVars env, TaskListener listener) {
@@ -134,19 +247,80 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         }
     }
 
+    private static String getLatestVersion(TaskListener listener) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        logMessage(listener, "Current working directory: " + System.getProperty("user.dir"));
+        File file = new File("./demo/src/main/java/io/jenkins/plugins/sample/categories.json");
+
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("text/plain");
+//            RequestBody body = RequestBody.create(mediaType, "");
+            Request request = new Request.Builder()
+                    .url("https://anypoint.mulesoft.com/exchange/api/v2/assets/a541ecba-4afe-4ce2-a4bb-7c8849912c7f/deom/asset")
+                    .addHeader("Authorization", "bearer 24c7e417-60a2-4821-b69b-617efef0c00d")
+                    .build();
+            Response response = client.newCall(request).execute();
+
+
+
+            // Log request details
+            logMessage(listener,"Request URL: " + request.url());
+            logMessage(listener,"Request Method: " + request.method());
+            logMessage(listener,"Request Headers: " + request.headers());
+
+            // Log response details
+            logMessage(listener,"Response Status Code: " + response.code());
+            logMessage(listener,"Response Headers: " + response.headers());
+//            System.out.println("Response Body: " + response.body().string());
+
+            String responseBody = response.body().string();
+            logMessage(listener,responseBody);
+            String latestVersion = extractVersion(listener, responseBody);
+
+            logMessage(listener,"Latest version: " + latestVersion);
+
+            // Close the response body
+            response.close();
+            return latestVersion;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    private static String extractVersion(TaskListener listener, String jsonResponse) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+            return jsonNode.get("version").toString();
+        } catch (Exception e) {
+            logMessage(listener,"Error in extracting version: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private static String getAccessToken(TaskListener listener) {
+
+        return "";
+    }
+
     @Symbol("greet")
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
         public FormValidation doCheckName(@QueryParameter String value, @QueryParameter boolean useFrench)
                 throws IOException, ServletException {
-            if (value.length() == 0)
-                return FormValidation.error(Messages.HelloWorldBuilder_DescriptorImpl_errors_missingName());
-            if (value.length() < 4)
-                return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_tooShort());
-            if (!useFrench && value.matches(".*[éáàç].*")) {
-                return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench());
-            }
+//            if (value.length() == 0)
+//                return FormValidation.error(Messages.HelloWorldBuilder_DescriptorImpl_errors_missingName());
+//            if (value.length() < 4)
+//                return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_tooShort());
+//            if (!useFrench && value.matches(".*[éáàç].*")) {
+//                return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench());
+//            }
             return FormValidation.ok();
         }
 
@@ -157,7 +331,8 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 
         @Override
         public String getDisplayName() {
-            return Messages.HelloWorldBuilder_DescriptorImpl_DisplayName();
+//            return Messages.HelloWorldBuilder_DescriptorImpl_DisplayName();
+            return "";
         }
     }
 }
